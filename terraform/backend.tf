64418ec42db3dc75b8dc911328bbe92177c46f0f
Jeyanigeswar <nicknikesh10@gmail.com>
2026-06-08 16:49:41 +0000

    Initial Terraform project structure

---
+++ terraform/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+
+  backend "s3" {
+
+    bucket         = "terraform-state-bucket"
+    key            = "eks-platform/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+
+    encrypt = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+
+  type = string
+
+  default = "ap-south-1"
+}
+
+variable "environment" {
+
+  type = string
+
+  default = "dev"
+}
